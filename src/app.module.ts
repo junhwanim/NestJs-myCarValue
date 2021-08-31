d8b4ccb050637a9dbd76f546247261109fa13e4b
Junhwan Im <junhwanim@hotmail.com>
2021-08-31 17:02:24 -0400

    Dealing with User Data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+// import TypeORM
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [],
+  // import TypeOrmModule.forRoot({}): connection to SQLite DB
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      // SQLite is a file based database: store all the inforamtion
+      // related to your database inside of one single file.
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
